4d96f2ce3a24cb9aec94fe0777ded0ef555ef382
adeismlg <102838271+adeismlg@users.noreply.github.com>
2025-09-17 14:56:10 +0700

    fix

--- bin/bmi_calculator.dart
+++ bin/bmi_calculator.dart
@@ -1,5 +1,34 @@
-import 'package:bmi_calculator/bmi_calculator.dart' as bmi_calculator;
 
-void main(List<String> arguments) {
-  print('Hello world: ${bmi_calculator.calculate()}!');
+void addToHistory(List<Map<String, dynamic>> riwayat, Map<String, dynamic> data) {
+	riwayat.add(data);
+}
+
+void tampilkanRiwayat(List<Map<String, dynamic>> riwayat) {
+	print('Riwayat Perhitungan BMI:');
+	for (var data in riwayat) {
+		print('Tinggi: [34m${data['tinggi']} cm[0m, Berat: [32m${data['berat']} kg[0m, BMI: [33m${data['bmi'].toStringAsFixed(2)}[0m, Kategori: [35m${data['kategori']}[0m');
+	}
+}
+
+void main() {
+	List<Map<String, dynamic>> riwayat = [];
+
+	// Simulasi beberapa input
+	var inputs = [
+		{'tinggi': 170.0, 'berat': 65.0},
+		{'tinggi': 165.0, 'berat': 75.0},
+		{'tinggi': 180.0, 'berat': 85.0},
+	];
+
+	for (var input in inputs) {
+		double tinggi = input['tinggi'] as double;
+		double berat = input['berat'] as double;
+		if (validateInput(tinggi, berat)) {
+			var hasil = calculateBMI(tinggi, berat);
+			addToHistory(riwayat, hasil);
+		}
+	}
+
+	// Tampilkan riwayat menggunakan perulangan
+	tampilkanRiwayat(riwayat);
 }
